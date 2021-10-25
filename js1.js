d27b3396e7cf787e1ebc192231ed7f3e98e59950
Iryna Kutsenko <79517296+irynakutsenko@users.noreply.github.com>
2021-10-25 11:53:32 +0200

    Natural Parks of  USA
    Final project of module 01 of Bootcamp
    Page of Natural Parks of USA

---
+++ js1.js
@@ -0,0 +1,8 @@
+// let parks;
+
+
+// fetch("https://developer.nps.gov/api/v1/parks?api_key=UFBlil5Sslf5ypeVBgTpPdgm4hxtvOSkQ10Tw33d").then(function(result) {
+//     return result.json();
+// }).then(function(json) {
+//     parks = json;
+// });
